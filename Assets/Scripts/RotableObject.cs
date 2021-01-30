c5140c043de1c3a5c5b3470d913e70c2e48b6dc8
guidoc73 <41069450+guidoc73@users.noreply.github.com>
2021-01-30 01:51:25 -0300

    Mirror done

---
+++ Assets/Scripts/RotableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RotableObject
+{
+    bool _isPlayingAnimation;
+    private readonly Vector3 _axisToRotate;
+
+    GameObject ObjectToRotate { get; }
+
+    public RotableObject(GameObject objectToRotate, Vector3 axisToRotate)
+    {
+        ObjectToRotate = objectToRotate;
+        _axisToRotate = axisToRotate;
+    }
+
+    public bool IsPlayingAnimation()
+    {
+        return _isPlayingAnimation;
+    }
+
+    public IEnumerator TemporaryAnimationForward(float initAngle, float endAngle)
+    {
+        _isPlayingAnimation = true;
+        float interpolation = 0;
+
+        while (ObjectToRotate.transform.rotation != Quaternion.AngleAxis(endAngle, _axisToRotate))
+        {
+            interpolation += Time.deltaTime;
+            ObjectToRotate.transform.rotation = Quaternion.Lerp(Quaternion.AngleAxis(initAngle, _axisToRotate), Quaternion.AngleAxis(endAngle, _axisToRotate), interpolation);
+            yield return new WaitForEndOfFrame();
+        }
+
+        ObjectToRotate.transform.rotation = Quaternion.AngleAxis(endAngle, _axisToRotate);
+
+        _isPlayingAnimation = false;
+    }
+}
