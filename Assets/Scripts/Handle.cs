fb26660cfb518be8b882f4eceec47353312ed990
guidoc73 <41069450+guidoc73@users.noreply.github.com>
2021-01-30 01:04:59 -0300

    Handler, Pressplatform, PusheableRock, IActivable and IActivator done.

---
+++ Assets/Scripts/Handle.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Handle : MonoBehaviour, IActivator
+{
+    bool _isActivated;
+    bool _isPlayingAnimation;
+
+    private Quaternion _startRotation;
+
+    [SerializeField] private GameObject handlePivot;
+    [SerializeField] private Activatable activatable;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _startRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Action();
+    }
+
+    public void Action()
+    {
+        if (!_isPlayingAnimation)
+        {
+            TemporaryMoveHandle();
+            Execute();
+        }
+    }
+
+    public void Execute()
+    {
+        activatable.Execute();
+    }
+
+    private void TemporaryMoveHandle()
+    {
+        if (_isActivated)
+        {
+            StartCoroutine(TemporaryAnimation(-1));
+        }
+        else
+        {
+            StartCoroutine(TemporaryAnimation(1));
+        }
+
+        _isActivated = !_isActivated;
+    }
+
+    IEnumerator TemporaryAnimation(float time)
+    {
+        _isPlayingAnimation = true;
+        float interpolation = 0;
+
+        while (handlePivot.transform.rotation != Quaternion.AngleAxis(-35 * time, Vector3.right))
+        {
+            interpolation += Time.deltaTime;
+            handlePivot.transform.rotation = Quaternion.Lerp(Quaternion.AngleAxis(35 * time, Vector3.right), Quaternion.AngleAxis(-35 * time, Vector3.right), interpolation);
+            yield return new WaitForEndOfFrame();
+        }
+
+        _isPlayingAnimation = false;
+    }
+}
---
+++ Assets/Scripts/PressPlatform.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PressPlatform : MonoBehaviour, IActivator
+{
+    private Vector3 _startPosition;
+
+    public float pressedPosition = 1;
+
+    public MeshRenderer mesh;
+
+    public Activatable activatable;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != 10)
+        {
+            mesh.transform.position = _startPosition - Vector3.down * pressedPosition;
+            Execute();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != 10)
+        {
+            mesh.transform.position = _startPosition;
+            Execute();
+        }
+    }
+
+    public void Execute()
+    {
+        activatable.Execute();
+    }
+}
