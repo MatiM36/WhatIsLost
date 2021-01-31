251ebbb6ce48fc71cda5fb8b16308c96309af039
guidoc73 <41069450+guidoc73@users.noreply.github.com>
2021-01-31 12:55:08 -0300

    Added UI Interactable Buttons

---
+++ Assets/Scripts/UIInteractableButtons.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInteractableButtons : MonoBehaviour
+{
+    public UIEnum uiType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponentInParent<Player>();
+
+        if (player)
+            ShowUI(player);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponentInParent<Player>();
+
+        if (player)
+            HideUI(player);
+    }
+
+    public void ShowUI(Player player)
+    {
+        if (uiType == UIEnum.Jump)
+            player.view.ShowJumpUI();
+        else
+            player.view.ShowMoveUI();
+    }
+
+    public void HideUI(Player player)
+    {
+        if (uiType == UIEnum.Jump)
+            player.view.HideJumpUI();
+        else
+            player.view.HideMoveUI();
+    }
+}
+
+public enum UIEnum
+{
+    Jump,
+    Move,
+}
