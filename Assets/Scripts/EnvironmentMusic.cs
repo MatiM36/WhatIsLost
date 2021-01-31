da997bf8989a4c9120e87385aff7648e62a71c4a
guidoc73 <41069450+guidoc73@users.noreply.github.com>
2021-01-30 22:42:02 -0300

    Added environment music

---
+++ Assets/Scripts/EnvironmentMusic.cs
@@ -0,0 +1,13 @@
+﻿using Mati36.Vinyl;
+using UnityEngine;
+
+public class EnvironmentMusic : MonoBehaviour
+{
+    public VinylAsset environmentMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        environmentMusic.Play();
+    }
+}
